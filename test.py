1da8a69e34b81f722a7c09b55353f4144eb0b485
Sergei2000 <dsdem@mail.ru>
2022-12-07 13:06:05 +0300

    Initial

---
+++ test.py
@@ -0,0 +1,144 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+
+
+text = """
+
+THE PRESIDENT:  Today, a jury in Minnesota found former Minneapolis Police Officer Derek Chauvin guilty on all counts in the murder of George Floyd last May.
+
+It was a murder in the full light of day, and it ripped the blinders off for the whole world to see the systemic racism the Vice President just referred to — the systemic racism that is a stain our nation’s soul; the knee on the neck of justice for Black Americans; the profound fear and trauma, the pain, the exhaustion that Black and brown Americans experience every single day.
+
+The murder of George Floyd launched a summer of protest we hadn’t seen since the Civil Rights era in the ‘60s — protests that unified people of every race and generation in peace and with purpose to say, “Enough.  Enough.  Enough of the senseless killings.”
+
+Today — today’s verdict is a step forward.  I just spoke with the Governor of Minnesota, who thanked me for the close work with his team. 
+
+And I also just spoke with George Floyd’s family again — a remarkable family of extraordinary courage.  Nothing can ever bring their brother, their father back.  But this can be a giant step forward in the march toward justice in America.
+
+Let’s also be clear that such a verdict is also much too rare.  For so many people, it seems like it took a unique and extraordinary convergence of factors: a brave young woman with a smartphone camera; a crowd that was traumatized — traumatized witnesses; a murder that lasts almost 10 minutes in broad daylight for, ultimately, the whole world to see; officers standing up and testifying against a fellow officer instead of just closing ranks, which should be commended; a jury who heard the evidence, carried out their civic duty in the midst of an extraordinary moment, under extraordinary pressure.
+
+For so many, it feels like it took all of that for the judicial system to deliver a just — just basic accountability.
+
+We saw how traumatic and exhausting just watching the trial was for so many people.  Think about it, those of you who are listening — think about how traumatic it was for you.  You weren’t there.  You didn’t know any of the people. 
+
+But it was difficult, especially for the witnesses who had to relive that day.
+
+It’s a trauma on top of the fear so many people of color live with every day when they go to sleep at night and pray for the safety of themselves and their loved ones.
+
+Again — as we saw in this trial, from the fellow police officers who testified — most men and women who wear the badge
+serve their communities honorably.
+
+But those few who fail to meet that standard
+must be held accountable.  And they were today; one was.  No one should be above the law.  And today’s verdict sends that message.
+
+But it is not enough.  We can’t stop here.
+
+In order to deliver real change and reform, we can and we must do more to reduce the likelihood that tragedies like this will ever happen and occur again; to ensure that Black and brown people or anyone — so they don’t fear the interactions with law enforcement, that they don’t have to wake up knowing that they can lose their very life in the course of just living their life.  They don’t have to worry about whether their sons or daughters will come home after a grocery store run or just walking down the street or driving their car or playing in the park or just sleeping at home.
+
+And this takes acknowledging and confronting, head on, systemic racism and the racial disparities that exist in policing and in our criminal justice system more broadly. 
+
+You know, state and local government and law enforcement needs to step up, but so does the federal government.  That’s why I have appointed the leadership at the Justice Department that I have, that is fully committed to restoring trust between law enforcement and the community they are sworn to serve and protect.  I have complete confidence in the Attorney General — General Garland’s leadership and commitment. 
+
+I have also nominated two key Justice Department nominees — Vanita Gupta and Kristen Clarke — who are eminently qualified, highly respected lawyers who have spent their entire careers fighting to advance racial equity and justice. 
+
+Vanita and Kristen have the experience and the skill necessary to advance our administration’s priorities to root out unconstitutional policing and reform our criminal justice system, and they deserve to be confirmed. 
+
+We also need Congress to act.  George Floyd was murdered almost a year ago.  There’s meaningful police reform legislation in his name.  You just heard the Vice President speak of it.  She helped write it.  Legislation to tackle systemic misconduct in police departments, to restore trust between law enforcement and the people that are entrusted to serve and protect.  But it shouldn’t take a whole year to get this done.
+
+In my conversations with the Floyd family — and I spoke with them again today — I assured them that we’re going to continue to fight for the passage of the George Floyd Justice in Policing Act so we can — I can sign it into law as quickly as possible.  And there’s more to do. 
+
+Finally, it’s the work we do every day to change hearts and minds as well as laws and policies — that’s the work we have to do.  Only then will full justice and full equality be delivered to all Americans.  And that’s what I just discussed with the Floyd family. 
+
+The guilty verdict does not bring back George.  But through the family’s pain, they are finding purpose so George’s — George’s legacy will not be just about his death, but about what we must do in his memory.
+
+I also spoke to Gianna — George’s (inaudible) — George’s young daughter, again.  When I met her last year — I’ve said this before — at George’s funeral, I told her how brave I thought she was.  And I, sort of, knelt down to hold her hand.  I said, “Daddy’s looking down on you.  He’s so proud.”  She said to me then — I’ll never forget it — “Daddy changed the world.”
+
+Well, I told her this afternoon, “Daddy did change the world.”  Let that be his legacy: a legacy of peace, not violence — of justice. 
+
+Peaceful expression of that legacy are inevitable and appropriate, but violent protest is not.  And there are those who will seek to exploit the raw emotions of the moment — agitators and extremists who have no interest in social justice; who seek to carry out violence, destroy property, to fan the flames of hate and division; who will do everything in their power to stop this country’s march toward racial justice.  We can’t let them succeed.
+
+This is the time for this country to come together, to unite as Americans.  There can never be any safe harbor for hate in America.
+
+I’ve said it many times: The battle for the soul of this nation has been a constant push and pull for more than 240 years — a tug of war between the American ideal that we’re all created equal and the harsh reality that racism has long torn us apart.
+
+At our best, the American ideal wins out.  So we can’t leave this moment or look away, thinking our work is done.  We have to look at it — we have to — we have to look at it as we did for those 9 minutes and 29 seconds.  We have to listen.  “I can’t breathe.  I can’t breathe.”  Those were George Floyd’s last words.  We can’t let those words die with him.  We have to keep hearing those words.
+
+We must not turn away.  We can’t turn away.  We have a chance to begin to change the trajectory in this country.  It’s my hope and prayer that we live up to the legacy.
+
+May God bless you.  And may God bless the — George Floyd and his family.
+
+Thank you for taking the time to be here.  This can be a moment of significant change. 
+PRESIDENT BIDEN: Prime Minister, good to see you and have a chance to offer my congratulations in person again, in front of everyone here.
+
+And I think this meeting is an opportunity for us to reaffirm the incredible and close relationship between Great Britain and the United States. You are our closest ally and closest friend, and we appreciate that.
+
+We stand together with Ukraine and the Ukrainian people, and continue to take issue with Russian behavior. And we’re going to continue to support Ukraine and as long as Russia continues their aggression.
+
+And this morning, we’ve already met with our fellow NATO and G7 leaders to address Russia’s latest missile strikes against Kyiv and — and western Ukraine. And it’s just — it’s merciless. And it’s — I think it borders on — anyway, it’s — it’s way over the top what they’re doing.
+
+And at a moment when world leaders here in Bali are seeking to make progress on global peace, Spuki- — Putin is striking civilian targets and children and women. I mean, it’s just — it’s almost — in my words, not yours — barbaric, what he’s doing.
+
+And Russia can and should stop the war. They’re able to do it. They could stop it tomorrow if they wanted to.
+
+But I’m glad we’re on the same page in terms of supporting the Ukrainian people’s right to be free and — and — and free of all Russian forces in their country.
+
+So — but thank you. And I’m looking forward to our discussion.
+
+PRIME MINISTER SUNAK: Well, President Biden, it’s a great honor to have the opportunity to sit down with you. I think the partnership between our countries is unique and enduring, and of course, it’s bigger than any two individuals. But I’m proud to have stewardship of it at the moment.
+
+And as you said —
+
+PRESIDENT BIDEN: I’m glad you are too.
+
+PRIME MINISTER SUNAK: Yeah. Well, as you said, the United States is our closest ally, biggest trading partner, closest security partner. And I’m glad to see that partnership in action doing good, whether it’s standing up to — and I agree with your words — barbaric activity by Russia in Ukraine, but also tackling climate change, stabilizing the global economy.
+
+Our values are aligned, our interests are aligned, and there’s much good that we can do working together.
+
+I look forward to our discussion.
+
+PRESIDENT BIDEN: I’m looking forward to it too.
+
+Thank you all.
+
+PRIME MINISTER SUNAK: Thank you.
+
+1
+Thank you.
+"""
+
+import nltk
+from sklearn import decomposition
+from sklearn.decomposition import TruncatedSVD
+sentences = nltk.tokenize.sent_tokenize(text)
+
+
+
+#Ngram
+vectorizer = CountVectorizer(ngram_range=(1, 2),analyzer='char_wb')
+X = vectorizer.fit_transform(sentences)
+print(X.toarray())
+print(vectorizer.get_feature_names_out())
+
+
+#word
+vectorizer = CountVectorizer(ngram_range=(1, 1))
+X = vectorizer.fit_transform(sentences)
+print(X.toarray())
+print(vectorizer.get_feature_names_out())
+
+print("transform ")
+print(vectorizer.transform( ["which unconstitutional"]).toarray())
+
+#collocation
+vectorizer = CountVectorizer(ngram_range=(2, 3))
+X = vectorizer.fit_transform(sentences)
+print(X.toarray())
+print(len(X.toarray()),len(X.toarray()[0]))
+print(vectorizer.get_feature_names_out())
+
+pca = decomposition.PCA()
+pca.fit(X.toarray())
+X = pca.transform(X.toarray())
+print(X)
+print(len(X),len(X[0]))
+
